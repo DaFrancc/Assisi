03000230fbe2f728dddda893cbc2651ccf5c3979
DaFrancc <65142449+DaFrancc@users.noreply.github.com>
2026-02-13 18:56:48 -0600

    Conan and cmake seem to funciton now

--- modules/Math/include/Assisi/Math/GLM.hpp
+++ modules/Math/include/Assisi/Math/GLM.hpp
@@ -3,7 +3,7 @@
 #include <Assisi/Math/GLMConfig.hpp>
 
 /* This makes it much harder to include GLM “raw” without config. */
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/quaternion.hpp>
-#include <gtx/quaternion.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
